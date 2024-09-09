e10d6e6deca44f6c9abe17e755b02ee543264229
JYOTHI~ <koribillijyothi2002@msitprogram.net.com>
2024-09-09 17:41:17 +0530

    day6

---
+++ day2/Untitled-1.py
@@ -0,0 +1,24 @@
+# def fac(n):
+#     mul=1
+#     for i in range(1,n+1):
+#         mul=mul*i
+#     return mul
+# n=int(input())
+# print(fac(n))
+
+# def pow(base,exponent):
+#     n=base
+#     m=exponent
+#     return((n**m))
+# base=int(input())
+# exponent=int(input())
+# print(pow(base,exponent))
+
+# def prime(n):
+#     for i in range(2,n):
+#         if n%i==0:
+#             return False
+#     return True
+# n=int(input())
+# print(prime(n))
+
